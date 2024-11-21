cb4996ec0631d35d18ec220dad69e707174f53fd
Vlasosik <128188585+Vlasosik@users.noreply.github.com>
2024-11-21 20:41:37 +0200

    added start command to run bot.

--- src/bot/command/StartCommand.cpp
+++ src/bot/command/StartCommand.cpp
@@ -1,5 +1,8 @@
 #include "bot/command/StartCommand.h"
 
+#include "bot/ui_manager/UIManager.h"
+
 void StartCommand::Execute(TgBot::Bot &bot, const TgBot::Message::Ptr &message) {
     bot.getApi().sendMessage(message->chat->id, "Привіт! Я ваш фінансовий бот 🤖💲");
-}
+    UIManager::sendMainBar(bot, message->chat->id);
+};
