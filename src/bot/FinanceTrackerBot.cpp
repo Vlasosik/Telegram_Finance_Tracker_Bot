bae6d5a04abd5268d80a3e408e7e3e3a579f014c
Vlasosik <128188585+Vlasosik@users.noreply.github.com>
2024-11-21 20:43:23 +0200

    updated finance tracker bot class added a few main methods for better jobs is it

--- src/bot/FinanceTrackerBot.cpp
+++ src/bot/FinanceTrackerBot.cpp
@@ -2,10 +2,99 @@
 
 #include <tgbot/net/TgLongPoll.h>
 
+#include "bot/command/BackCommand.h"
+#include "bot/command/HelpCommand.h"
+#include "bot/command/InfoCommand.h"
+#include "bot/command/StartCommand.h"
+#include "bot/ui_manager/UIManager.h"
+
 FinanceTrackerBot::FinanceTrackerBot(const std::string &token) : bot(token) {
+    registerCommand();
+    registerCallbacks();
 }
 
-[[noreturn]] void FinanceTrackerBot::run() const {
+void FinanceTrackerBot::registerCommand() {
+    commands["/start"] = std::make_shared<StartCommand>();
+    commands["/help"] = std::make_shared<HelpCommand>();
+    commands["/info"] = std::make_shared<InfoCommand>();
+    // made command info
+}
+
+void FinanceTrackerBot::registerCallbacks() {
+    callbackQueries["Категорії"] = [this](const TgBot::CallbackQuery::Ptr &query) {
+        UIManager::sendCategoryBar(bot, query->message->chat->id);
+    };
+    callbackQueries["Їжа"] = [this](const TgBot::CallbackQuery::Ptr &query) {
+        userSelectedCategories[query->message->chat->id].emplace(query->data);
+        bot.getApi().sendMessage(query->message->chat->id, "Категорію \"" + query->data + "\" успішно додано.");
+    };
+    callbackQueries["Одяг та взуття"] = [this](const TgBot::CallbackQuery::Ptr &query) {
+        userSelectedCategories[query->message->chat->id].emplace(query->data);
+        bot.getApi().sendMessage(query->message->chat->id, "Категорію \"" + query->data + "\" успішно додано.");
+    };
+    callbackQueries["Сімʼя та діти"] = [this](const TgBot::CallbackQuery::Ptr &query) {
+        userSelectedCategories[query->message->chat->id].emplace(query->data);
+        bot.getApi().sendMessage(query->message->chat->id, "Категорію \"" + query->data + "\" успішно додано.");
+    };
+    callbackQueries["Накопичення"] = [this](const TgBot::CallbackQuery::Ptr &query) {
+        userSelectedCategories[query->message->chat->id].emplace(query->data);
+        bot.getApi().sendMessage(query->message->chat->id, "Категорію \"" + query->data + "\" успішно додано.");
+    };
+    callbackQueries["Житло"] = [this](const TgBot::CallbackQuery::Ptr &query) {
+        userSelectedCategories[query->message->chat->id].emplace(query->data);
+        bot.getApi().sendMessage(query->message->chat->id, "Категорію \"" + query->data + "\" успішно додано.");
+    };
+    callbackQueries["Транспорт"] = [this](const TgBot::CallbackQuery::Ptr &query) {
+        userSelectedCategories[query->message->chat->id].emplace(query->data);
+        bot.getApi().sendMessage(query->message->chat->id, "Категорію \"" + query->data + "\" успішно додано.");
+    };
+    callbackQueries["Особисті витрати"] = [this](const TgBot::CallbackQuery::Ptr &query) {
+        userSelectedCategories[query->message->chat->id].emplace(query->data);
+        bot.getApi().sendMessage(query->message->chat->id, "Категорію \"" + query->data + "\" успішно додано.");
+    };
+    callbackQueries["Освіта та розвиток"] = [this](const TgBot::CallbackQuery::Ptr &query) {
+        userSelectedCategories[query->message->chat->id].emplace(query->data);
+        bot.getApi().sendMessage(query->message->chat->id, "Категорію \"" + query->data + "\" успішно додано.");
+    };
+    callbackQueries["Назад"] = [this](const TgBot::CallbackQuery::Ptr &query) {
+        UIManager::sendMainBar(bot, query->message->chat->id);
+    };
+}
+
+void FinanceTrackerBot::handleCommandMessage(const std::string &command, const TgBot::Message::Ptr &message) {
+    if (commands.contains(command)) {
+        commands[command]->Execute(bot, message);
+    }else {
+        bot.getApi().sendMessage(message->chat->id, "Невідома команда: " + command);
+    }
+}
+
+void FinanceTrackerBot::handleCallbackMessages(const std::string &command, const TgBot::Message::Ptr &message) {
+    if (callbackMessages.contains(command)) {
+        callbackMessages[command](message);
+    } else {
+        bot.getApi().sendMessage(message->chat->id, "Невідома команда: " + command);
+    }
+}
+
+void FinanceTrackerBot::handleCallbackQuery(const std::string &command, const TgBot::CallbackQuery::Ptr &query) {
+    if (callbackQueries.contains(command)) {
+        callbackQueries[command](query);
+    } else {
+        bot.getApi().sendMessage(query->message->chat->id, "Невідома команда: " + command);
+    }
+}
+
+void FinanceTrackerBot::Run() {
+    bot.getEvents().onCommand({"start", "help", "info"},
+                              [this](const TgBot::Message::Ptr &message) {
+                                  handleCommandMessage(message->text, message);
+                              });
+
+    bot.getEvents().onCallbackQuery([this](const TgBot::CallbackQuery::Ptr &query) {
+        handleCallbackQuery(query->data, query);
+    });
+
     TgBot::TgLongPoll long_poll(bot);
     std::cout << bot.getApi().getMyName()->name << ": started.\n";
     while (true) {
