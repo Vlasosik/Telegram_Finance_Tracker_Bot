b554c08a925504d2b23fb6ac130504817d7c04a3
Vlasosik <128188585+Vlasosik@users.noreply.github.com>
2024-11-21 14:42:07 +0200

    added class UIManager for the separation of logic buttons.

---
+++ include/bot/ui_manager/UIManager.h
@@ -0,0 +1,8 @@
+#ifndef UIMANAGER_H
+#define UIMANAGER_H
+class UIManager {
+public:
+    void sendMainBar(int64_t userId);
+    void sendCategoryBar(int64_t userId);
+};
+#endif //UIMANAGER_H
---
+++ src/bot/ui_manager/UIManager.cpp
@@ -0,0 +1,5 @@
+#include "bot/ui_manager/UIManager.h"
+
+void UIManager::sendMainBar(int64_t userId) {
+
+}
