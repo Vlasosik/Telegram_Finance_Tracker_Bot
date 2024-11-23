be0137dfedbc9117e7734652c2f92ad8c0ecc5d0
Vlasosik <128188585+Vlasosik@users.noreply.github.com>
2024-11-23 15:24:23 +0200

    removed back, category and help command.

--- src/bot/command/BackCommand.cpp
+++
@@ -1,8 +0,0 @@
-#include "bot/command/BackCommand.h"
-
-#include "bot/ui_manager/UIManager.h"
-
-void BackCommand::Execute(TgBot::Bot &bot, const TgBot::Message::Ptr &message) {
-    bot.getApi().sendMessage(message->chat->id, "Ви повернулися до головного меню.");
-    UIManager::sendMainBar(bot, message->chat->id);
-}
--- src/bot/command/HelpCommand.cpp
+++
@@ -1,6 +0,0 @@
-#include "bot/command/HelpCommand.h"
-
-void HelpCommand::Execute(TgBot::Bot &bot, const TgBot::Message::Ptr &message) {
-    bot.getApi().sendMessage(message->chat->id,
-                             "Якщо виникли несправності з ботом, прошу напишіть сюди✏️\n @R3qp1o");
-}
