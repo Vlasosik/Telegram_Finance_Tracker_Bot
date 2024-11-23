8a124f676e70e2c9d874eac1f675b4459c1a8592
Vlasosik <128188585+Vlasosik@users.noreply.github.com>
2024-11-23 15:24:23 +0200

    removed back, category and help command.

--- include/bot/command/BackCommand.h
+++
@@ -1,9 +0,0 @@
-#ifndef BACKCOMMAND_H
-#define BACKCOMMAND_H
-#include "Command.h"
-
-class BackCommand : public Command{
-public:
-    void Execute(TgBot::Bot &bot, const TgBot::Message::Ptr &message) override;
-};
-#endif //BACKCOMMAND_H
--- include/bot/command/HelpCommand.h
+++
@@ -1,9 +0,0 @@
-#ifndef HELPCOMMAND_H
-#define HELPCOMMAND_H
-#include "Command.h"
-
-class HelpCommand : public Command {
-public:
-    void Execute(TgBot::Bot &bot, const TgBot::Message::Ptr &message) override;
-};
-#endif //HELPCOMMAND_H
