961561e2efc97ce90895eb058c2216c77bf7d797
Vlasosik <128188585+Vlasosik@users.noreply.github.com>
2024-11-24 22:55:34 +0200

    add class add transaction.

---
+++ include/bot/callback_command/CallbackAddTransaction.h
@@ -0,0 +1,9 @@
+#ifndef CALLBACKADDTRANSACTION_H
+#define CALLBACKADDTRANSACTION_H
+#include "CallbackCommand.h"
+
+class CallbackAddTransaction : public CallbackCommand{
+public:
+    void ExecuteCallback(TgBot::Bot &bot, const TgBot::CallbackQuery::Ptr &query) override;
+};
+#endif //CALLBACKADDTRANSACTION_H
