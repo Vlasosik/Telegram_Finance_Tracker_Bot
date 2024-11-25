cc1faa800439822dbd8d82786f5709d84934280a
Vlasosik <128188585+Vlasosik@users.noreply.github.com>
2024-11-25 22:24:01 +0200

    refactored class CallbackSelectedCategories.

--- src/bot/callback_command/CallbackSelectedCategories.cpp
+++ src/bot/callback_command/CallbackSelectedCategories.cpp
@@ -4,9 +4,15 @@
 
 void CallbackSelectedCategories::ExecuteCallback(TgBot::Bot &bot, const TgBot::CallbackQuery::Ptr &query) {
     int64_t userId = query->message->chat->id;
-    userManager.getUser(userId);
-    // userManager.setState(userId, UserStateType::SELECTING_CATEGORY);
-    userManager.addCategory(userId, query->data);
-    auto message = bot.getApi().sendMessage(
-        userId, "Категорія " + userManager.getUserSelectedCategory(userId) + "  додана.");
+    try {
+        auto &userManager = UserManager::getInstance();
+        userManager.getUser(userId);
+        userManager.setState(userId, UserStateType::SELECTING_CATEGORY);
+        userManager.addCategory(userId, query->data);
+        auto message = bot.getApi().sendMessage(
+            userId, "Категорія " + userManager.getUserSelectedCategory(userId) + "  додана.");
+    } catch (std::invalid_argument &ex) {
+        auto message = bot.getApi().sendMessage(
+            userId, "Введіть категорію яка є у списку.");
+    }
 }
