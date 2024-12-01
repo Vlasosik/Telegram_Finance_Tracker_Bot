9d05c57e3dc34bb1cd651e0a76ec8937ca91433e
Vlasosik <128188585+Vlasosik@users.noreply.github.com>
2024-12-01 22:26:30 +0200

    created migration files for migrate file sql.

--- include/migration/MigrationSQL.h
+++ include/migration/MigrationSQL.h
@@ -1,8 +1,18 @@
-//
-// Created by Vlasik Pototskyi on 01.12.2024.
-//
-
 #ifndef MIGRATIONSQL_H
 #define MIGRATIONSQL_H
+#include <sqlpp11/postgresql/postgresql.h>
+#include <__filesystem/directory_iterator.h>
+#include <fstream>
+#include <string>
+class MigrationSQL {
+    std::string migrationPath;
+    sqlpp::postgresql::connection db;
 
+public:
+    MigrationSQL();
+    MigrationSQL(const std::string &user, const std::string &password, const std::string &dbname,
+                 int port, const std::string &host);
+
+    void executeMigrations(const std::string &migrationFolder);
+};
 #endif //MIGRATIONSQL_H
--- src/migration/MigrationSQL.cpp
+++ src/migration/MigrationSQL.cpp
@@ -1,3 +1,57 @@
-//
-// Created by Vlasik Pototskyi on 01.12.2024.
-//
+#include "migration/MigrationSQL.h"
+
+MigrationSQL::MigrationSQL(const std::string &user, const std::string &password, const std::string &dbname, int port,
+                           const std::string &host) {
+    sqlpp::postgresql::connection_config config;
+    config.user = user;
+    config.password = password;
+    config.dbname = dbname;
+    config.port = port;
+    config.host = host;
+    db = sqlpp::postgresql::connection(config);
+}
+
+// constructor for used migrations file in environment variables
+MigrationSQL::MigrationSQL() {
+    sqlpp::postgresql::connection_config config;
+
+    const char *db_user = std::getenv("DB_USER");
+    const char *db_password = std::getenv("DB_PASSWORD");
+    const char *db_name = std::getenv("DB_NAME");
+    const char *db_port = std::getenv("DB_PORT");
+    const char *db_host = std::getenv("DB_HOST");
+
+    if (db_user) config.user = db_user;
+    if (db_password) config.password = db_password;
+    if (db_name) config.dbname = db_name;
+    if (db_port) config.port = std::stoi(db_port);
+    if (db_host) config.host = db_host;
+
+    db = sqlpp::postgresql::connection(config);
+}
+
+void MigrationSQL::executeMigrations(const std::string &migrationFolder) {
+    try {
+        std::filesystem::path exe_path = std::filesystem::current_path();
+        std::cout << "Поточний каталог: " << exe_path << std::endl;
+        std::filesystem::path migration_path = exe_path / migrationFolder;
+        if (!exists(migration_path) || !is_directory(migration_path)) {
+            std::cerr << "Папка міграцій не знайдена: " << migration_path << std::endl;
+            return;
+        }
+        for (const auto &entry: std::filesystem::directory_iterator(migration_path)) {
+            if (entry.is_regular_file() && entry.path().extension() == ".sql") {
+                if (std::ifstream migration_file(entry.path()); migration_file) {
+                    std::string migration_sql((std::istreambuf_iterator<char>(migration_file)),
+                                              std::istreambuf_iterator<char>());
+                    db.execute(migration_sql);
+                    std::cout << "Міграція виконана успішно: " << entry.path() << std::endl;
+                } else {
+                    std::cerr << "Не вдалося відкрити файл: " << entry.path() << std::endl;
+                }
+            }
+        }
+    } catch (const std::exception &e) {
+        std::cerr << "Помилка при виконанні міграцій: " << e.what() << std::endl;
+    }
+}
