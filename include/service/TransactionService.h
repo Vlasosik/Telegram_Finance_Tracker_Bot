7047cf876190a1d108624548de50cff48d8faf83
Vlasosik <128188585+Vlasosik@users.noreply.github.com>
2024-12-02 18:50:52 +0200

    created transcation service for jobs to date base.

---
+++ include/service/TransactionService.h
@@ -0,0 +1,36 @@
+#ifndef TRANSACTIONSERVICE_H
+#define TRANSACTIONSERVICE_H
+#include <sqlpp11/detail/enable_if.h>
+#include <sqlpp11/postgresql/connection.h>
+#include <sqlpp11/postgresql/connection_config.h>
+
+#include "transactions/Transaction.h"
+
+typedef std::chrono::system_clock::time_point time_point;
+
+class TransactionService {
+    sqlpp::postgresql::connection db;
+
+public:
+    TransactionService();
+    void createTransaction(int64_t userId, const std::string &category, double amount, time_point date);
+
+    std::optional<int64_t> getTransactionId(int64_t id);
+
+    void updateTransaction(int64_t userId, const std::string &category, double amount, time_point date);
+
+    void deleteTransaction(int64_t userId);
+
+    [[nodiscard]] std::vector<Transaction> getListTransactionByUserId(int64_t userId);
+
+    std::vector<Transaction> getListTransactionForWeek(int64_t userId);
+
+    double getSumTransactionForWeek(int64_t userId);
+
+    std::vector<Transaction> getListTransactionForMonth(int64_t userId);
+
+    double getSumTransactionForMonth(int64_t userId);
+
+    bool isTransactionExist(int64_t id);
+};
+#endif //TRANSACTIONSERVICE_H
